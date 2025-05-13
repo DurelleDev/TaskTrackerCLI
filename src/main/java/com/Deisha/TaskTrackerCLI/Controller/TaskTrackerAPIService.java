ace4c98be1c31c94c23f8cf89b3d10518e8d3096
Durelle <47389740+DurelleDev@users.noreply.github.com>
2025-05-13 10:02:32 +0200

    Add files via upload

---
+++ src/main/java/com/Deisha/TaskTrackerCLI/Controller/TaskTrackerAPIService.java
@@ -0,0 +1,14 @@
+package com.Deisha.TaskTrackerCLI.Controller;
+
+import com.Deisha.TaskTrackerCLI.Model.TaskTracker;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasktracker")
+public class TaskTrackerAPIService {
+
+    private TaskTracker taskTracker;
+
+    public TaskTracker getTaskTrackerDetails
+}
---
+++ src/main/java/com/Deisha/TaskTrackerCLI/Model/TaskTracker.java
@@ -0,0 +1,63 @@
+package com.Deisha.TaskTrackerCLI.Model;
+
+import org.springframework.scheduling.config.Task;
+
+public class TaskTracker {
+    private String taskId;
+    private String taskDescription;
+    private int status;
+    private String taskDateCreated;
+    private String taskUpdatedDate;
+
+    public TaskTracker(){
+    }
+
+    public TaskTracker(String taskId, String taskDescription, int status, String taskDateCreated, String taskUpdatedDate){
+
+        this.taskId = taskId;
+        this.taskDescription = taskDescription;
+        this.status =status;
+        this.taskDateCreated = taskDateCreated;
+        this.taskUpdatedDate = taskUpdatedDate;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTaskDateCreated() {
+        return taskDateCreated;
+    }
+
+    public void setTaskDateCreated(String taskDateCreated) {
+        this.taskDateCreated = taskDateCreated;
+    }
+
+    public String getTaskUpdatedDate() {
+        return taskUpdatedDate;
+    }
+
+    public void setTaskUpdatedDate(String taskUpdatedDate) {
+        this.taskUpdatedDate = taskUpdatedDate;
+    }
+}
